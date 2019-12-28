effbbdd3ce206041f3b8c179cb086cc36dcab4ca
icebreaker99 <yangqiming@moteng.ltd>
2019-12-28 13:22:49 +0800

    add prismjs highlighting library

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
   <nuxt />
 </template>
-
+<script>
+if (process.client) {
+  require("prismjs/themes/prism-tomorrow.css")
+  import("prismjs").then(Prism => {
+    Prism.highlightAll()
+  })
+}
+</script>
 <style lang="scss"></style>
